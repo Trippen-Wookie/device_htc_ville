55d2722ad859792479ad9269cb8ec29302e34a2f
Jalen Battle-Bryant <flashalot.dev@gmail.com>
2015-06-22 19:07:06 -0400

    CandyLife
    Signed-off-by: Jalen Battle-Bryant <flashalot.dev@gmail.com>

--- candy5.mk
+++ candy5.mk
@@ -5,15 +5,15 @@ PRODUCT_RELEASE_NAME := ville
 TARGET_SCREEN_HEIGHT := 960
 TARGET_SCREEN_WIDTH := 540
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Candy5 stuff.
+$(call inherit-product, vendor/candy5/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/htc/ville/device_ville.mk)
 
 # Device naming
 PRODUCT_DEVICE := ville
-PRODUCT_NAME := cm_ville
+PRODUCT_NAME := candy5_ville
 PRODUCT_BRAND := htc
 PRODUCT_MODEL := One S
 PRODUCT_MANUFACTURER := HTC
